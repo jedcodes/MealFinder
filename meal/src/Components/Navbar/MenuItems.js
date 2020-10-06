af080937100493c40f3d7b8f28544c2b2b532aa1
jackdelamou <jackdelamou.97@gmail.com>
2020-10-06 22:55:47 +0200

    navbar
    working on making navbar responsiv

---
+++ meal/src/Components/Navbar/MenuItems.js
@@ -0,0 +1,8 @@
+export const MenuItems = [
+  { title: "Home", url: "/", cName: "navbar__links" },
+  { title: "Breakfast", url: "/breakfast", cName: "navbar__links" },
+  { title: "Lunch", url: "/lunch", cName: "navbar__links" },
+  { title: "Dinner", url: "/dinner", cName: "navbar__links" },
+  { title: "Drinks", url: "/drinks", cName: "navbar__links" },
+  { title: "Sign In", url: "/signin", cName: "navbar__links--mobile" },
+];
--- meal/src/Components/Navbar/Navbar.jsx
+++ meal/src/Components/Navbar/Navbar.jsx
@@ -1,55 +1,33 @@
-import React from "react";
-import { NavLink } from "react-router-dom";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { RiMenu3Fill, RiCloseLine } from "react-icons/ri";
+import { MenuItems } from "./MenuItems";
+import { Button } from "../Button/Button";
 
 const Navbar = () => {
+  const [click, setClick] = useState(false);
+
+  const clickHandler = () => setClick(!click);
+  const closeMobileMenu = () => setClick(!click);
+
   return (
-    <div className="navbar">
+    <nav className="navbar">
       <div className="navbar__logo">MEAL PLANER</div>
-      <div className="navbar__items">
-        <NavLink
-          activeClassName="current__link"
-          className="navbar__item"
-          to="/"
-        >
-          <li>HOME</li>
-        </NavLink>
-        <NavLink
-          className="navbar__item"
-          to="/breakfast"
-          activeClassName="current__link"
-        >
-          <li>BREAKFAST</li>
-        </NavLink>
-        <NavLink
-          className="navbar__item"
-          to="/lunch"
-          activeClassName="current__link"
-        >
-          <li>LUNCH</li>
-        </NavLink>
-        <NavLink
-          className="navbar__item"
-          to="/dinner"
-          activeClassName="current__link"
-        >
-          <li>DINNER</li>
-        </NavLink>
-        <NavLink
-          className="navbar__item"
-          to="/drinks"
-          activeClassName="current__link"
-        >
-          <li>DRINKS</li>
-        </NavLink>
-        <NavLink
-          className="navbar__item"
-          to="/"
-          activeClassName="current__link"
-        >
-          <li>BUILD</li>
-        </NavLink>
+      <div className="navbar__mobileIcon" onClick={clickHandler}>
+        {click ? <RiCloseLine /> : <RiMenu3Fill />}
       </div>
-    </div>
+      <ul className={click ? "navbar__menu active" : "navbar__menu"}>
+        {MenuItems.map((item, index) => {
+          return (
+            <li key={index}>
+              <Link className={item.cName} to={item.url}>
+                {item.title}
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
+    </nav>
   );
 };
 
